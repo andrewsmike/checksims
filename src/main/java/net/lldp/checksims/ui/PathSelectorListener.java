3765b76bbf79f7281da8d2f3c2dd4ce2a4524f5b
Michael Andrews <mandrews@wpi.edu>
2015-11-21 00:17:38 -0500

    Allow selection of files and directories.

--- src/main/java/net/lldp/checksims/ui/PathSelectorListener.java
+++ src/main/java/net/lldp/checksims/ui/PathSelectorListener.java
@@ -16,7 +16,7 @@ public class PathSelectorListener implements ActionListener
     {
         this.path = path;
         
-        //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
         fc.setCurrentDirectory(new java.io.File("."));
         fc.setDialogTitle("title");
         fc.setAcceptAllFileFilterUsed(false);
